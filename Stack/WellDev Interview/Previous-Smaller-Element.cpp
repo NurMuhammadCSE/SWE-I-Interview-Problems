1404780025e10e3159a72e7ff62bf77f8cd05bdb
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2025-03-17 22:21:15 +0600

    Next-Smaller-Greater-Element Added

---
+++ Stack/WellDev Interview/Previous-Smaller-Element.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    vector<int> result(n, -1);
+    stack<int> st;
+
+    for(int i=0; i<n; i++){
+        while(!st.empty() and st.top() >= arr[i]){
+            st.pop();
+        }
+
+        if(!st.empty()){
+            result[i] = st.top();
+        }
+
+        st.push(arr[i]);
+    }
+    return result;
+}
+
+
+#include<bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    vector<int> result(n);
+    stack<int> st;
+    st.push(-1);
+
+    for(int i=0; i<n; i++){
+        while(st.top() >= arr[i]){
+            st.pop();
+        }
+
+        result[i] = st.top();
+
+        st.push(arr[i]);
+    }
+    return result;
+}
+
